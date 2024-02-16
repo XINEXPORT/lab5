f89c1b7b074a7b6796415e7a63367d1f7007fe54
Christine Hoang <40744735+XINEXPORT@users.noreply.github.com>
2024-02-15 21:19:34 -0600

    Update hoanglab5.R

--- hoanglab5.R
+++ hoanglab5.R
@@ -13,6 +13,11 @@ ggplot(ramen, aes(x = Style, y = Stars, color = Style)) + geom_boxplot()
 #Modify the plot you just created to only use data from the USA.
 ggplot(filter(ramen, Country == "USA"), aes(x = Style, y = Stars, color = Style))+geom_boxplot()
 
+#Ramen bars in the USA are highly rated.
+#Boxed ramen are the lowest rated which could be due to lower quality.
+#Bowl, cup, pack, and tray ramen vary in ratings.
+#Canned ramen have an average rating.
+
 #Create a new tibble that only contains data from Japan, India, Taiwan, and the USA. 
 #Then, create a boxplot that shows the distribution of stars by style for each country on a subplot with two subplots per row.
 
@@ -22,6 +27,10 @@ filtered_ramen<-ramen %>%
 filtered_ramen
 
 ggplot(filtered_ramen, aes(x = Style, y = Stars, color = Style)) + geom_boxplot() + facet_wrap(~Country, nrow = 2)
+
+#Ramen bars are only popular in the USA.
+#India only consumes packs and cups of ramen in which it receives mid-range ratings.
+#The quality boxed ramen in Japan is higher than in the USA.
 
 #Create a histogram of the tibble you created in step 5 that uses 12 bins to display the distribution of the Stars values.
 ggplot(filtered_ramen, aes(x = Stars, fill = Style)) +
